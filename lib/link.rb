22c987b88c15e0a982cec3bf95e59fc5d483c2fa
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-02-13 13:01:26 +0000

    Install pg gem and use to connect to bookmark_manager database in link.rb. Pass

--- lib/link.rb
+++ lib/link.rb
@@ -1,9 +1,10 @@
+require 'pg'
+
 class Link
 
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.google.com",
-      "http://www.facebook.com" ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM links")
+    result.map { |link| link['url'] }
   end
 end
