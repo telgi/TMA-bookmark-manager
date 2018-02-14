04a0c2e0278c2aba519c913b0380135606ebd66b
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-02-14 16:00:50 +0000

    refactor link class to use .query class method from database connection

--- lib/link.rb
+++ lib/link.rb
@@ -1,24 +1,13 @@
-require 'pg'
+require 'database_connection'
 
 class Link
 
   def self.all
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-
-    result = connection.exec("SELECT * FROM links")
+    result = DatabaseConnection.query("SELECT * FROM links")
     result.map { |link| link['url'] }
   end
 
   def self.add(new_link)
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'bookmark_manager_test')
-    else
-      connection = PG.connect(dbname: 'bookmark_manager')
-    end
-    connection.exec("INSERT INTO links (url) VALUES('#{new_link[:url]}')")
+    DatabaseConnection.query("INSERT INTO links (url) VALUES('#{new_link[:url]}')")
   end
 end
