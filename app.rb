179fb9f76eda8088289b2acaa19fc71918f9c9af
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-15 16:54:06 +0000

    Achieved greatness - flash message comes up as temporary box that can be closed

--- app.rb
+++ app.rb
@@ -4,21 +4,25 @@ require './lib/link'
 require './spec/database_connection_setup'
 
 class BookmarkManager < Sinatra::Base
-
   register Sinatra::Flash
 
   enable :sessions
 
   get '/' do
     @links = Link.all
-    flash[:warning] = 'Hooray, Dan sucks!'
     erb(:index)
   end
 
   post '/add_link' do
-    Link.add(params[:url])
-    redirect '/'
+    @new_link = params[:url]
+    if Link.valid_url?(@new_link)
+      Link.add(@new_link)
+      redirect '/'
+    else
+      flash[:warning] = 'This is not a valid URL'
+      redirect '/'
+    end
   end
 
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,7 +1,6 @@
 feature "viewing links" do
   scenario "User can see links" do
     visit '/'
-    click_button "Show links"
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"
     expect(page).to have_content "http://www.facebook.com"
